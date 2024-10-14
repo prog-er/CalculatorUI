bb5cc033d68ee8a86be692d6519dc9a8ff34f84f
prog-er <olji20020 000@gmail.com>
2024-10-14 10:06:52 +0500

    Initial Commit

---
+++ CalculatorUI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CalculatorUI
+//
+//  Created by Olzhas Karibayev on 14.10.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ CalculatorUITests/CalculatorUITests.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorUITests.swift
+//  CalculatorUITests
+//
+//  Created by Olzhas Karibayev on 14.10.2024.
+//
+
+import Testing
+@testable import CalculatorUI
+
+struct CalculatorUITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
